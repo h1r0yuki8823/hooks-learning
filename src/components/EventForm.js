ec309ed458a646879de8fe1b9ebbc87854441fa6
野崎裕之 <nozakihiroyuki@nozakihiroyukinoMacBook-Pro.local>
2019-12-20 16:14:26 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,10 +2,13 @@ import React,{useContext, useState,} from 'react'
 
 import {
     CREATE_EVENT,
-    DELETE_ALL_EVENTS
+    DELETE_ALL_EVENTS,
+    ADD_OPERATION_LOG,
+    DELETE_OPERATION_LOGS
 } from '../actions/index'
 
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () =>{
     const { state, dispatch } = useContext(AppContext)
@@ -22,6 +25,12 @@ const EventForm = () =>{
           title,
           body
         })
+
+        dispatch({
+            type:ADD_OPERATION_LOG,
+            description: 'イベントを作成しました。',
+            operatedAt: timeCurrentIso8601()
+        })
   
         setTitle('')
         setBody('')
@@ -30,7 +39,15 @@ const EventForm = () =>{
     const deleteALlEvents = e => {
       e.preventDefault()
       const result = window.confirm('全てのイベントを本当に削除しても良いですか?')
-      if(result)dispatch({type:DELETE_ALL_EVENTS})
+      if(result){
+        dispatch({type:DELETE_ALL_EVENTS})
+
+        dispatch({
+            type:ADD_OPERATION_LOG,
+            description:'全てのイベントを削除しました。',
+            operatedAt: timeCurrentIso8601()
+        })
+      }
     }
   
     const unCreatable = title === '' || body === ''
